f13da2c0b89121705434de172b9a15e4d8709d61
Ed1l3udo <edileudommfilho@gmail.com>
2025-07-30 22:52:11 -0300

    Player jump

---
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 40f;
+    private Rigidbody2D rb;
+    private bool jumpRequest = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpRequest = true; 
+        }
+    }
+    void FixedUpdate()
+    {
+        if (jumpRequest)
+        {
+            rb.linearVelocity = Vector2.up * jumpForce; 
+            jumpRequest = false;
+        }
+    }
+
+}
